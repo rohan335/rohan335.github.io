5ec77d29f10d0cebd0c2d2950a421d93744a0f02
rohan335 <rohandesai335@gmail.com>
2023-02-08 17:29:09 -0500

    treasure hunt poc

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function check() {
+
+    switch (document.getElementById('answer').value.toUpperCase()) {
+      case "10":
+        alert("You did it! Continue your journey at the middle school cafeteria...")
+        break;
+    
+      case "ADA LOVELACE":
+        alert("Ding ding ding! You can find the next clue at a Spanish teacher's door.")
+        break;
+
+      case "123":
+        alert("Woohoo! One more puzzle remains: you can find it at the vending machine")
+        break;
+      default:
+        alert('WRONG! Try again...');
+        break;
+    }
+
+    if(document.getElementById('answer').value == "100") {
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <meta name="viewport" content="width=device-width, user-scalable=no" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>What's your answer? (⊙_⊙)</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          <input type="text" id='answer' className='text'></input>
+          <button onClick={check} className="button">Try it!</button>
+        </div>
       </header>
     </div>
   );
